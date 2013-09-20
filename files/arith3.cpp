caebc149df15d3a3f805b38b309bdc3526050309
Axel Wagner <mail@merovius.de>
2013-09-20 04:58:45 +0200

    Lektion 7 hinzugefügt

---
+++ files/arith3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Gebe eine Zahl ein: ";
+    int a;
+    std::cin >> a;
+
+    std::cout << "Gebe noch eine Zahl ein: ";
+    int b;
+    std::cin >> b;
+
+    std::cout << "Ihre Summe ist " << a + b << std::endl;
+}
