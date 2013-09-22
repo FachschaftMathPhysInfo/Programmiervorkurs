0ecfeaf02be78ebf8704e4e6ffbf8f7fed8b7936
Axel Wagner <mail@merovius.de>
2013-09-23 00:56:47 +0200

    Lektion 16 hinzugefügt

---
+++ files/warnings.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cout << "Gebe eine Zahl ein: " << std::endl;
+    std::cin >> n;
+
+    if (n = 0) {
+        std::cout << "Null!" << std::endl;
+    } else {
+        std::cout << "Nicht Null!" << std::endl;
+    }
