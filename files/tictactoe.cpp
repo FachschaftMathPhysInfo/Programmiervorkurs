a339ff8a3cfb97f64754332f25d6db115d9e303d
Axel Wagner <mail@merovius.de>
2013-09-25 21:08:06 +0200

    Lektion 17 zugefügt

---
+++ files/tictactoe.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+extern int frage_feld_nummer(int feld[9]);
+// Fügt hier die anderen Funktionen ein
+
+int main() {
+    // Setup
+    while (true) {
+        // Input
+        // Update
+        // Display
+    }
+}
