deada12ffbf6890bf0340a4386564abf0ba45373
Friedrich Schwedler <friedrich+git@mathphys.fsk.uni-heidelberg.de>
2017-09-23 22:23:34 +0200

    zeilenumbrüche damit es in pdf passt

--- files/variablen.cpp
+++ files/variablen.cpp
@@ -5,8 +5,9 @@ int main(){
     std::string beschreibung = "wundervoll";
 
     std::cout << "Hallo, mein Tag war " << beschreibung << std::endl;
-    std::cout << "Ich habe " << beschreibung << " aus einem Skript gelernt"
-        << std::endl;
-    std::cout << "Ich hoffe dir geht es genauso " << beschreibung << std::endl;
+    std::cout << "Ich habe " << beschreibung
+        << " aus einem Skript gelernt" << std::endl;
+    std::cout << "Ich hoffe dir geht es genauso "
+        << beschreibung << std::endl;
     return 0;
 }
