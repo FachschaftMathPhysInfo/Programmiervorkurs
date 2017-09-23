e3ee3ebc54604ddcae82f7a77b3b73d6736fc34c
Friedrich Schwedler <friedrich+git@mathphys.fsk.uni-heidelberg.de>
2017-09-23 17:45:13 +0200

    variablen name `einarray` -> `einvector`

--- files/vector.cpp
+++ files/vector.cpp
@@ -4,19 +4,19 @@
 #include <vector>
 
 int main() {
-    std::vector<int> einarray = std::vector<int>(100);
+    std::vector<int> einvector= std::vector<int>(100);
 
     srand(time(NULL));
 
     int i = 0;
     while (i < 100) {
-        einarray[i] = rand();
+        einvector[i] = rand();
         i = i + 1;
     }
 
-    std::cout << "An Index 0 steht " << einarray[0] << std::endl;
-    std::cout << "An Index 42 steht " << einarray[42] << std::endl;
-    std::cout << "An Index 99 steht " << einarray[99] << std::endl;
+    std::cout << "An Index 0 steht " << einvector[0] << std::endl;
+    std::cout << "An Index 42 steht " << einvector[42] << std::endl;
+    std::cout << "An Index 99 steht " << einvector[99] << std::endl;
 
     return 0;
 }
