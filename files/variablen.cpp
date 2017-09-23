8f76cf3fff03d33979143b2c55c3936866513074
jenuk <gandalf.d@web.de>
2017-09-23 20:17:47 +0200

    Neue Version

---
+++ files/variablen.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string beschreibung = "wundervoll";
+
+    std::cout << "Hallo, mein Tag war " << beschreibung << std::endl;
+    std::cout << "Ich habe aus einem " << beschreibung
+        << " Skript gelernt" << std::endl;
+    std::cout << "Ich hoffe dir geht es genauso " << beschreibung << std::endl;
+    return 0;
+}
