f9bc524393f26b2db6b0818161ade7262ba04857
jenuk <gandalf.d@web.de>
2017-09-23 22:27:20 +0200

    Variablen (#68)
    * Neue Version
    
    * feedback tom
    
    * praxis teil hinzugefügt
    
    * kleinkram
    
    * zeilenumbrüche damit es in pdf passt
    
    * .cpp dateien aktualisert
    
    * wird nicht mehr verwendet

---
+++ files/variablen.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string beschreibung = "wundervoll";
+
+    std::cout << "Hallo, mein Tag war " << beschreibung << std::endl;
+    std::cout << "Ich habe " << beschreibung
+        << " aus einem Skript gelernt" << std::endl;
+    std::cout << "Ich hoffe dir geht es genauso "
+        << beschreibung << std::endl;
+    return 0;
+}
