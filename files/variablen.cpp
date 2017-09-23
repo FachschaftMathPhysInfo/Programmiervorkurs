4e2b33a5a6c625250f2fbff6dee3d686b419e775
jenuk <gandalf.d@web.de>
2017-09-23 20:39:33 +0200

    feedback tom

--- files/variablen.cpp
+++ files/variablen.cpp
@@ -5,8 +5,8 @@ int main(){
     std::string beschreibung = "wundervoll";
 
     std::cout << "Hallo, mein Tag war " << beschreibung << std::endl;
-    std::cout << "Ich habe aus einem " << beschreibung
-        << " Skript gelernt" << std::endl;
+    std::cout << "Ich habe " << beschreibung << " aus einem Skript gelernt"
+        << std::endl;
     std::cout << "Ich hoffe dir geht es genauso " << beschreibung << std::endl;
     return 0;
 }
