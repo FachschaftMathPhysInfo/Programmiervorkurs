38930a77633ab7c5d3edd155a5bc59a52df09c2f
Christian Heusel <christian@heusel.eu>
2019-08-10 12:49:22 +0200

    replaced tabs with spaces and fixed indentation
    Signed-off-by: Christian Heusel <christian@heusel.eu>

--- files/runden2.cpp
+++ files/runden2.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
 
 enum Runden{
-	mathe,
-	ab,
-	auf
+    mathe,
+    ab,
+    auf
 };
 
 int runden(double d, Runden mode){
-	 int i = d; //speichert Double in Integer, schneidet Kommastellen ab
+    int i = d; // speichert Double in Integer, schneidet Kommastellen ab
     if(mode == mathe){
-       if(d - i < 0.5){
-		     return i;
-		 }
-		 else {
-			 return i + 1;
-		 }
-	 }
-	 else if (mode == ab){
-		 return i;
-	 }
-	 else if (mode == auf){
-		 return i + 1;
-	 }
-	 else {
-		 return 0; //Hier sollte ein Fehler geworfen werden. Das könnt ihr noch nicht
-	 }
+        if(d - i < 0.5){
+            return i;
+        }
+        else {
+            return i + 1;
+        }
+    }
+    else if (mode == ab){
+        return i;
+    }
+    else if (mode == auf){
+        return i + 1;
+    }
+    else {
+        return 0; // Hier sollte ein Fehler geworfen werden. Das könnt ihr noch nicht
+    }
 }
 
 int main(){
-	 //hier lässt sich sehr(!) intuitiv erkennen, was das Programm tut
+    // hier lässt sich sehr(!) intuitiv erkennen, was das Programm tut
     std::cout << runden(2.7, mathe) << std::endl;
-	 std::cout << runden(2.3, mathe) << std::endl;
-	 std::cout << runden(2.7, ab) << std::endl;
-	 std::cout << runden(2.7, auf) << std::endl;
+    std::cout << runden(2.3, mathe) << std::endl;
+    std::cout << runden(2.7, ab) << std::endl;
+    std::cout << runden(2.7, auf) << std::endl;
+    return 0;
 }
