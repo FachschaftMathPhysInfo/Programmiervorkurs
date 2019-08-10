fa5b919bbb3bc73a7024e99a544f021ee6fbc7df
Christian Heusel <christian@heusel.eu>
2019-08-10 13:01:24 +0200

    some feedback from GitHub
    Signed-off-by: Christian Heusel <christian@heusel.eu>

--- files/runden2.cpp
+++ files/runden2.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
 enum Runden{
-    mathe,
+    kaufmaennisch,
     ab,
     auf
 };
 
 int runden(double d, Runden mode){
-    int i = d; // speichert Double in Integer, schneidet Kommastellen ab
-    if(mode == mathe){
+    // speichert Double als Integer -> schneidet Kommastellen ab
+    int i = d;
+    if(mode == kaufmaennisch){
         if(d - i < 0.5){
             return i;
         }
@@ -23,15 +24,16 @@ int runden(double d, Runden mode){
         return i + 1;
     }
     else {
-        return 0; // Hier sollte ein Fehler geworfen werden. Das könnt ihr noch nicht
+        // Hier sollte ein Fehler geworfen werden.
+        // Das könnt ihr aber leider noch nicht :(
+        return 0;
     }
 }
 
 int main(){
     // hier lässt sich sehr(!) intuitiv erkennen, was das Programm tut
-    std::cout << runden(2.7, mathe) << std::endl;
-    std::cout << runden(2.3, mathe) << std::endl;
+    std::cout << runden(2.7, kaufmaennisch) << std::endl;
+    std::cout << runden(2.3, kaufmaennisch) << std::endl;
     std::cout << runden(2.7, ab) << std::endl;
     std::cout << runden(2.7, auf) << std::endl;
-    return 0;
 }
