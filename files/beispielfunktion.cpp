09c267eda6884f546b5781ff0c9aae448b9e966e
Felix <59015017+whateven-username@users.noreply.github.com>
2021-09-26 21:01:45 +0200

    Apply suggestions from code review
    Co-authored-by: Jonas Müller <jonasjmueller@googlemail.com>

--- files/beispielfunktion.cpp
+++ files/beispielfunktion.cpp
@@ -1,14 +1,14 @@
-double my_func(double x, int n){
+double my_func(double x, int n) {
     int ergebnis = x;
     int i = 1;
-    while(i <= n){
+    while (i <= n) {
 	ergebnis = ergebnis * x ;
         i = i + 1;
     } 
     return ergebnis;
 }
 
-int main(int argc, char *argv[]){
+int main() {
     double ergebnis1 = my_func(3.142, 4);
     double ergebnis2 = my_func(ergebnis1, 2);
     double ergebnis3 = my_func(5.0 + my_func(3.0, 2), 3);
