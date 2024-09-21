7330fd9dd2371f596f9b0bf3da8b297f88210625
Nicolas Schledorn <107950614+fschledorn@users.noreply.github.com>
2024-09-21 22:42:12 +0200

    Inhaltliche Änderungen für den Vorkurs 2024 (#128)
    * initial commit fuer eigen computer
    
    * added eigene computer part to main script
    
    * small ansible changes
    
    * updated readme #124
    
    * Section für Windows leicht überarbeitet und aktualisiert auf WSL2.
    
    * Update build.yml
    
    Updated from upload-artifact@v2 to @v4, as @v3 will be deprecated as of Nov. 30 2024 (1,5months)
    
    * Windows section aktualisiert, hello world angepasst auf vscode
    
    * In Kapitel 10 werden jetzt for und while Loops vorgestellt. Außerdem wurden Aufgaben hinzugefügt um for-loops zu benutzen und einen for-loop in einen while-loop umzuwandeln
    
    * kleine rechtschreibkorrektur im diagramm
    
    * Dopplung entfernt
    
    * Kapitel Vektoren leicht überarbeitet: Einen while-loop durch einen for-loop ersetut, kleine aufgabenstellung hinzugefügt zur verhinderung von out-of bounce zugriffen. kleine fehlerkorrektur in prim-for-loop
    
    * preprocessing hinzugefügt
    
    * huch ist mir ein fehler durchgerutscht
    
    * added missing escape symbol
    
    * rechtschreibfehler korrigiert
    
    Co-authored-by: Christian Heusel <christian@heusel.eu>
    
    * clarification
    
    Co-authored-by: Christian Heusel <christian@heusel.eu>
    
    * Delete vorkurs_log.txt
    
    no idea why this got committed
    
    * renamed lesson 18
    
    * of course i forgot to edit the script to include the new name for lesson 18
    
    * Update basics/schleifen.tex
    
    * rechtschreibfehler und clion footnote hinzugefügt
    
    * Update hello_world.tex
    
    fix broken link
    
    ---------
    
    Co-authored-by: Felix <github@elsermail.com>
    Co-authored-by: holyaltbier <mail@dheidemann.de>
    Co-authored-by: Christian Heusel <christian@heusel.eu>
    Co-authored-by: Tim Grube <115377141+Plebysnacc@users.noreply.github.com>
    Co-authored-by: Felix Schledorn <107950614+fschledorn@users.noreply.github.com>

---
+++ files/prim-for-loop.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cout << "Gebe eine (positive) Zahl ein: ";
+    std::cin >> n;
+
+    if (n <= 0) {
+        std::cerr << "Die Zahl soll positiv sein!" << std::endl;
+        // Wir benutzen return, um unser Programm vorzeitig
+        // abzubrechen. 1 bedeutet, dass ein Fehler aufgetreten
+        // ist, 0 bedeutet, alles ist okay
+        return 1;
+    }
+
+    bool hat_teiler = false;
+    // Äquivaltent zu while (i < n)
+    for (int i = 2; i < n; i++) {
+        if ((n % i) == 0) {
+            hat_teiler = true;
+        }
+    }
+
+    if (hat_teiler) {
+        std::cout << n << " ist keine Primzahl" << std::endl;
+    }
+    else {
+        std::cout << n << " ist eine Primzahl" << std::endl;
+    }
+    return 0;
+}
--- files/vector.cpp
+++ files/vector.cpp
@@ -8,17 +8,15 @@ int main() {
 
     srand(time(NULL));
 
-    int i = 0;
-    while (i < 100) {
+    for (int i = 0; i < 100; i++) {
         einvector.push_back(rand());
-        i = i + 1;
     }
     std::cout << "An Index  0 steht " << einvector[0] << std::endl;
     std::cout << "An Index 42 steht " << einvector[42] << std::endl;
     std::cout << "An Index 99 steht " << einvector[99] << std::endl;
 
     // und nun alle werte auf 42 setzen ;-)
-    i = 0;
+    int i = 0;
     while (i < einvector.size()) {
         einvector[i] = 42;
         i = i + 1;
