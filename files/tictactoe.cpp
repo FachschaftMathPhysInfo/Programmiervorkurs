388ce4999ca5b91e642598cebed999f76120494e
Nicolas Schledorn <107950614+fschledorn@users.noreply.github.com>
2024-09-30 11:21:56 +0200

    Update files/tictactoe.cpp
    Co-authored-by: Tim Grube <115377141+Plebysnacc@users.noreply.github.com>

--- files/tictactoe.cpp
+++ files/tictactoe.cpp
@@ -2,7 +2,7 @@
 #include <array>
 
 int frage_feld_nummer(std::array<int ,9> feld){
-  // Hier wird die Funktion implementiert
+  // Implementiere hier die Abfrage nach dem gespielten Feld //
 }
 
 void gebe_feld_aus(std::array<int ,9>){
