7b66dbaf767b19be35ccc21585b96c07104c5e76
Nick75 <nsmolkin@hotmail.com>
2024-11-26 10:12:31 +0100

    Moved files; changed files/fizzbuzz.cpp

--- appendix/fizzbuzz.cpp
+++
@@ -1,6 +0,0 @@
-#include <string>
-#include <iostream>
-int main() {std::string a="";
-for(int i=1;i<=100;i++){a="";if(i%3==0){a+="Fizz";}
-if(i%5==0){a+="Buzz";}if(a==""){a=std::to_string(i);}
-std::cout<<a<<std::endl;}}
