f252872d276f6bca16ce39d2d2bad27e61cbcc76
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2025-04-15 21:58:47 +0200

    change >= 0 to > 0 in final check

--- .github/workflows/test_scripts/check_compilability.py
+++ .github/workflows/test_scripts/check_compilability.py
@@ -3,8 +3,9 @@ import subprocess
 
 BASE_PATH = "vorkurs/"
 
+
 def find_all_cpp_files():
-    #vorkurs_path = pathlib.Path(BASE_PATH)
+    # vorkurs_path = pathlib.Path(BASE_PATH)
     vorkurs_path = pathlib.Path("/home/jan/Coding/Ende/Programmiervorkurs/vorkurs")
     cpp_files = set(vorkurs_path.rglob("*.cpp"))
     broken_cpp_files = set(vorkurs_path.rglob("*broken.cpp"))
@@ -12,29 +13,34 @@ def find_all_cpp_files():
 
     return [str(cpp_file) for cpp_file in working_cpp_files]
 
+
 def test_single_cpp_file(file_path):
     compile_status = subprocess.getoutput(f"g++ {file_path}")
 
     if compile_status != "":
-        print("\n"+20*"=")
+        print("\n" + 20 * "=")
         print(f"Error in file {file_path}:\n")
         print(compile_status)
-        print(20*"="+"\n")
+        print(20 * "=" + "\n")
         return False
 
     return True
+
 
 def main():
     error_count = 0
     working_cpp_files = find_all_cpp_files()
 
     for cpp_file in working_cpp_files:
-        if not test_single_cpp_file(cpp_file): 
-            error_count += 1 
+        if not test_single_cpp_file(cpp_file):
+            error_count += 1
 
-    if error_count >= 0: 
+    if error_count > 0:
         print(f"::error::Found {error_count} non compiling files")
         exit(1)
+
+    exit(0)
+
 
 if __name__ == "__main__":
     main()
