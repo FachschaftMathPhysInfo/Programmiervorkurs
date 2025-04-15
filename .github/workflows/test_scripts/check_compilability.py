b84d9fbb252e65cc8e5632727373ff421124a319
Plebysnek <115377141+Plebysnacc@users.noreply.github.com>
2025-04-15 22:15:21 +0200

    add handling of deliberately broken files

--- .github/workflows/test_scripts/check_compilability.py
+++ .github/workflows/test_scripts/check_compilability.py
@@ -1,23 +1,28 @@
 import pathlib
 import subprocess
 
+# Thats where our cpps are currently
 BASE_PATH = "vorkurs/"
 
 
-def find_all_cpp_files():
-    # vorkurs_path = pathlib.Path(BASE_PATH)
+def find_all_cpp_files(findWorking):
     vorkurs_path = pathlib.Path("/home/jan/Coding/Ende/Programmiervorkurs/vorkurs")
-    cpp_files = set(vorkurs_path.rglob("*.cpp"))
     broken_cpp_files = set(vorkurs_path.rglob("*broken.cpp"))
-    working_cpp_files = cpp_files.difference(broken_cpp_files)
 
+    if not findWorking:
+        return [str(cpp_file) for cpp_file in broken_cpp_files]
+
+    cpp_files = set(vorkurs_path.rglob("*.cpp"))
+    working_cpp_files = cpp_files.difference(broken_cpp_files)
     return [str(cpp_file) for cpp_file in working_cpp_files]
 
 
-def test_single_cpp_file(file_path):
+def test_single_cpp_file(file_path, shouldCompile):
     compile_status = subprocess.getoutput(f"g++ {file_path}")
 
-    if compile_status != "":
+    if (compile_status != "" and shouldCompile) or (
+        compile_status == "" and not shouldCompile
+    ):
         print("\n" + 20 * "=")
         print(f"Error in file {file_path}:\n")
         print(compile_status)
@@ -28,15 +33,29 @@ def test_single_cpp_file(file_path):
 
 
 def main():
-    error_count = 0
-    working_cpp_files = find_all_cpp_files()
+    error_count_working = 0
+    error_count_broken = 0
+    working_cpp_files = find_all_cpp_files(True)
+    broken_cpp_files = find_all_cpp_files(False)
 
     for cpp_file in working_cpp_files:
-        if not test_single_cpp_file(cpp_file):
-            error_count += 1
+        if not test_single_cpp_file(cpp_file, True):
+            error_count_working += 1
 
-    if error_count > 0:
-        print(f"::error::Found {error_count} non compiling files")
+    for cpp_file in broken_cpp_files:
+        if not test_single_cpp_file(cpp_file, False):
+            error_count_broken += 1
+
+    if error_count_working > 0:
+        print(
+            f"::error::Found {error_count_working} non compiling files which should compile"
+        )
+        exit(1)
+
+    if error_count_broken > 0:
+        print(
+            f"::error:Found {error_count_broken} files that should be broken but compile"
+        )
         exit(1)
 
     exit(0)
