6134e5b6baec18630e56a9ede8324b2e31d260bf
Tim Grube <115377141+Plebysnacc@users.noreply.github.com>
2025-04-15 23:01:06 +0200

    Add compilation tests (#165)
    - Outsourced the tests into seperate python scripts
    - Added new compilability test

---
+++ .github/workflows/test_scripts/check_compilability.py
@@ -0,0 +1,65 @@
+import pathlib
+import subprocess
+
+# Thats where our cpps are currently
+BASE_PATH = "vorkurs/"
+
+
+def find_all_cpp_files(findWorking):
+    vorkurs_path = pathlib.Path(BASE_PATH)
+    broken_cpp_files = set(vorkurs_path.rglob("*broken.cpp"))
+
+    if not findWorking:
+        return [str(cpp_file) for cpp_file in broken_cpp_files]
+
+    cpp_files = set(vorkurs_path.rglob("*.cpp"))
+    working_cpp_files = cpp_files.difference(broken_cpp_files)
+    return [str(cpp_file) for cpp_file in working_cpp_files]
+
+
+def test_single_cpp_file(file_path, shouldCompile):
+    compile_status = subprocess.getoutput(f"g++ {file_path}")
+
+    if (compile_status != "" and shouldCompile) or (
+        compile_status == "" and not shouldCompile
+    ):
+        print("\n" + 20 * "=")
+        print(f"Error in file {file_path}:\n")
+        print(compile_status)
+        print(20 * "=" + "\n")
+        return False
+
+    return True
+
+
+def main():
+    error_count_working = 0
+    error_count_broken = 0
+    working_cpp_files = find_all_cpp_files(True)
+    broken_cpp_files = find_all_cpp_files(False)
+
+    for cpp_file in working_cpp_files:
+        if not test_single_cpp_file(cpp_file, True):
+            error_count_working += 1
+
+    for cpp_file in broken_cpp_files:
+        if not test_single_cpp_file(cpp_file, False):
+            error_count_broken += 1
+
+    if error_count_working > 0:
+        print(
+            f"::error::Found {error_count_working} non compiling files which should compile"
+        )
+        exit(1)
+
+    if error_count_broken > 0:
+        print(
+            f"::error:Found {error_count_broken} files that should be broken but compile"
+        )
+        exit(1)
+
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
