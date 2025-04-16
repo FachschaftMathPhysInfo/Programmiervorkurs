e0d1cd7a28a19d44100154edbbe48fb75d39cf28
Jan <MapManagement@protonmail.com>
2025-04-16 15:02:58 +0200

    Fix compilability script

--- .github/workflows/test_scripts/check_compilability.py
+++ .github/workflows/test_scripts/check_compilability.py
@@ -2,8 +2,7 @@ import pathlib
 import subprocess
 
 # Thats where our cpps are currently
-BASE_PATH = "vorkurs/"
-
+BASE_PATH = "files/"
 
 def find_all_cpp_files(findWorking):
     vorkurs_path = pathlib.Path(BASE_PATH)
@@ -25,15 +24,16 @@ def test_single_cpp_file(file_path, shouldCompile):
         print(f"Error in file {file_path}:\n")
         print(compile_status)
         print(20 * "=" + "\n")
+
         return False
     elif compile_status == "" and not shouldCompile:
         print("\n" + 20 * "=")
         print(f"File {file_path} compiles, but should be broken")
         print("\n" + 20 * "=")
+
         return False
 
     return True
-
 
 def main():
     error_count_working = 0
@@ -53,12 +53,13 @@ def main():
         print(
             f"::error::Found {error_count_working} non compiling files which should compile"
         )
-        exit(1)
-
+       
     if error_count_broken > 0:
         print(
-            f"::error:Found {error_count_broken} files that should be broken but compile"
+            f"::error::Found {error_count_broken} files that should be broken but compile"
         )
+
+    if error_count_broken > 0 or error_count_working > 0:
         exit(1)
 
     exit(0)
